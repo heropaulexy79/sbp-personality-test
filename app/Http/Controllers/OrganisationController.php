df6cba74fd27b0f1e67711182e16d6e595733804
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2024-03-13 12:41:01 +0100

    User and Organisation events, migrations

---
+++ app/Http/Controllers/OrganisationController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\EmployeeInvited;
+use App\Models\Organisation;
+use App\Models\OrganisationInvitation;
+use Illuminate\Http\Request;
+
+class OrganisationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Organisation $organisation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Organisation $organisation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Organisation $organisation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Organisation $organisation)
+    {
+        //
+    }
+
+
+
+
+
+
+    public function inviteEmployee(Request $request, Organisation $organisation)
+    {
+        // TODO: MAKE ROLE AN ENUM
+        if ($request->user()->role !== 'ADMIN') {
+            return abort(401, "You don't have permission to make this request");
+        }
+
+
+
+        $request->validate([
+            'email' => 'required|email|unique:users,email',
+            'role' => 'nullable|in:MEMBER,ADMIN',
+        ]);
+
+        $invitation = OrganisationInvitation::create([
+            'email' => $request->input('email'),
+            'organization_id' => $organisation->id,
+            'token' => OrganisationInvitation::generateUniqueToken(),
+            'role' => $request->input('role', 'MEMBER'),
+        ]);
+
+        // Send invitation email with the unique token
+        // Mail::to($invitation->email)->send(new UserInvitationEmail($invitation));
+
+        // Send Notification
+        event(new EmployeeInvited($invitation));
+
+        return back()->with('success', 'Employee invitation sent successfully!');
+    }
+}
---
+++ app/Models/Organisation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Organisation extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'name',
+    ];
+
+
+
+    public function employees()
+    {
+        return $this->hasMany(User::class, 'organisation_id');
+    }
+}
