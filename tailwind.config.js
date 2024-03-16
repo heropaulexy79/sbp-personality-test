8b78d4d158e63aac0a82fe0986a9faef93154060
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2024-03-16 03:26:56 +0100

    style fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,9 +19,9 @@ module.exports = {
         container: {
             center: true,
             padding: "2rem",
-            screens: {
-                "2xl": "1400px",
-            },
+            // screens: {
+            //     "2xl": "1400px",
+            // },
         },
         extend: {
             fontFamily: {
