3bd0fee5a0220e23207dcbcd29632bae2c005ebf
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2024-03-18 23:08:02 +0100

    clean up

--- resources/js/Pages/Organisation/Course/Lesson/Partials/use-quiz-manager.ts
+++ resources/js/Pages/Organisation/Course/Lesson/Partials/use-quiz-manager.ts
@@ -1,4 +1,4 @@
-import { ref, watch } from "vue";
+import { ref } from "vue";
 import { generateId } from "./utils";
 import { Question } from "@/types";
 
@@ -17,10 +17,6 @@ export function useQuizManager(initialQuestions?: Question[]) {
         });
     };
 
-    // const updateQuestion = (index: number, updatedQuestion: Question) => {
-    //     questions.value[index] = updatedQuestion;
-    // };
-
     const deleteQuestion = (index: number) => {
         questions.value.splice(index, 1);
     };
@@ -29,49 +25,18 @@ export function useQuizManager(initialQuestions?: Question[]) {
         questions.value[questionIndex].options.push({
             id: generateId(),
             text: "",
-            // isCorrect: false,
         });
     };
-
-    // const updateOption = (
-    //     questionIndex: number,
-    //     optionIndex: number,
-    //     updatedOption: QuestionOption
-    // ) => {
-    //     questions.value[questionIndex].options[optionIndex] = updatedOption;
-    // };
 
     const deleteOption = (questionIndex: number, optionIndex: number) => {
         questions.value[questionIndex].options.splice(optionIndex, 1);
     };
 
-    // const setCorrectAnswer = (questionIndex: number, optionIndex: number) => {
-
-    // };
-
-    // Helper to get a specific question's correct answer index
-    // const getCorrectAnswer = (question: Question) => {
-    //     return question.correctOption;
-    // };
-
-    // // Watch for changes in 'questions' and update the correct answer index
-    // watch(questions, () => {
-    //     questions.value.forEach((question) => {
-    //         const correctIndex = getCorrectAnswerIndex(question);
-    //         if (correctIndex !== -1) {
-    //             question.options[correctIndex].isCorrect = true;
-    //         }
-    //     });
-    // });
-
     return {
         questions,
         addQuestion,
-        // updateQuestion,
         deleteQuestion,
         addOption,
-        // updateOption,
         deleteOption,
-        // setCorrectAnswer,
     };
 }
