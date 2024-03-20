9a04665c44bd990fa321f4353a11375be6588325
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2024-03-20 14:44:54 +0100

    Course card

---
+++ resources/js/Pages/Course/Partials/CourseCard.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import { buttonVariants } from "@/Components/ui/button";
+import { cn } from "@/lib/utils";
+import { Course } from "@/types";
+import { Link } from "@inertiajs/vue3";
+import { ArrowRight } from "lucide-vue-next";
+
+defineProps<{ course: Pick<Course, "id" | "title"> }>();
+</script>
+
+<template>
+    <div
+        class="flex items-center justify-between rounded-lg p-4 transition-all shadow"
+    >
+        <div class="flex items-center gap-5">
+            <div class="size-32 bg-gray-400 rounded-md"></div>
+            <Link
+                :href="route('classroom.lesson.index', { course: course.id })"
+                :class="
+                    cn(
+                        buttonVariants({
+                            variant: 'link',
+                            size: 'lg',
+                            class: 'px-0',
+                        })
+                    )
+                "
+            >
+                <h3 class="text-lg font-medium">{{ course.title }}</h3>
+            </Link>
+        </div>
+
+        <div>
+            <!--  -->
+            <Link
+                :href="route('classroom.lesson.index', { course: course.id })"
+                :class="cn(buttonVariants({ size: 'lg' }))"
+            >
+                <span>Go to course</span>
+                <ArrowRight :size="16" />
+            </Link>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -2,13 +2,43 @@
 import LaravelPagination from "@/Components/ui/LaravelPagination.vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Course, Paginated } from "@/types";
-import { Head } from "@inertiajs/vue3";
+import { Head, Link, usePage } from "@inertiajs/vue3";
 import CreateOrganisationForm from "./Organisation/Partials/CreateOrganisationForm.vue";
+import CourseCard from "./Course/Partials/CourseCard.vue";
+
+const page = usePage();
 
 const props = defineProps<{
     courses: Paginated<Course>;
 }>();
 console.log(props);
+
+const courseNav = [
+    {
+        active:
+            page.props.query?.["status"] === "progress" ||
+            !page.props.query?.["status"],
+
+        href: route("dashboard", {
+            _query: {
+                ...page.props.query,
+                status: "progress",
+            },
+        }),
+        label: "In progress",
+    },
+    {
+        active: page.props.query?.["status"] === "completed",
+
+        href: route("dashboard", {
+            _query: {
+                ...page.props.query,
+                status: "completed",
+            },
+        }),
+        label: "Completed",
+    },
+];
 </script>
 
 <template>
@@ -32,19 +62,44 @@ console.log(props);
                 />
                 <div v-else class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <!-- <div
-                    class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
-                    >
-                    <div class="p-6 ">
-                        You're logged in!
-                    </div>
-                </div> -->
-                    Here are your courses
+                        class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
+                        >
+                        <div class="p-6 ">
+                            You're logged in!
+                        </div>
+                    </div> -->
+                    <!-- <h2 class="text-lg font-medium">Here are your courses</h2> -->
 
-                    <div v-for="course in courses.data">
-                        {{ course.title }}
-                    </div>
+                    <Transition name="fade" mode="out-in" appear>
+                        <div
+                            :key="$page.props.query?.['status']"
+                            class="mb-6 inline-flex h-9 items-center justify-center rounded-lg border border-primary bg-muted p-1 text-muted-foreground"
+                        >
+                            <Link
+                                v-for="item in courseNav"
+                                :data-state="
+                                    item.active ? 'active' : 'inactive'
+                                "
+                                :href="item.href"
+                                preserve-scroll
+                                class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-primary px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-primary data-[state=active]:text-primary-foreground data-[state=active]:shadow"
+                            >
+                                {{ item.label }}
+                            </Link>
+                        </div>
+                    </Transition>
 
-                    <LaravelPagination :items="courses" class="py-6" />
+                    <ul class="space-y-4">
+                        <li v-for="course in courses.data">
+                            <CourseCard :course="course" />
+                        </li>
+                    </ul>
+
+                    <LaravelPagination
+                        v-if="courses.total > courses.data.length"
+                        :items="courses"
+                        class="py-6 flex justify-center"
+                    />
                 </div>
             </div>
         </div>
