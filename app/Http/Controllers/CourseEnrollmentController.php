cd4fbc1f4dfa5fbd4b0adc91db39d8f5cd1aed59
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2024-04-18 18:27:24 +0100

    course enrollment and leaderboard page

--- app/Http/Controllers/CourseEnrollmentController.php
+++ app/Http/Controllers/CourseEnrollmentController.php
@@ -6,9 +6,44 @@ namespace App\Http\Controllers;
 
 use App\Models\Course;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CourseEnrollmentController extends Controller
 {
+
+    // TODO MOVE TO Namespaced Organisation/Course
+    public function show(Request $request, Course $course)
+    {
+        $user = $request->user();
+
+        $enrolledUsers  = $course->enrolledUsers()->where('organisation_id', $user->organisation_id)->get();
+
+        if (!$enrolledUsers || count($enrolledUsers) < 1) {
+            abort(404);
+        }
+
+        $students = [];
+
+        foreach ($enrolledUsers as $eu) {
+            $userScores = $eu->lessons()
+                ->select('user_lessons.score') // Select specific columns
+                ->get();
+
+            // dd(array_map(fn ($item) => $item->score, $userScores));
+
+            $students[] = [
+                'user' => $eu->only(['id', 'name']),
+                'score' => $userScores->sum('score'),
+                // 'score' => array_sum(array_map(fn($item)=>$item->score, $userScores)), // Array of lesson scores for the user
+                'scores' => $userScores, // Array of lesson scores for the user
+            ];
+        }
+
+        return Inertia::render('Organisation/Course/Leaderboard', [
+            "students" => $students,
+        ]);
+    }
+
     /**
      * Store a newly created resource in storage.
      */
@@ -17,11 +52,11 @@ class CourseEnrollmentController extends Controller
         $user = $request->user();
 
         // TODO: if course is public
-        if (! $course->is_published || ($course->organisation_id !== $user->organisation_id)) {
+        if (!$course->is_published || ($course->organisation_id !== $user->organisation_id)) {
             abort(401);
         }
 
-        if (! $course->enrolledUsers->contains($user)) {
+        if (!$course->enrolledUsers->contains($user)) {
             $course->enrolledUsers()->attach($user);
 
             // return response()->json(['message' => 'User successfully enrolled in the course!'], 201);
---
+++ resources/js/Pages/Organisation/Course/Leaderboard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import LaravelPagination from "@/Components/ui/LaravelPagination.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Course, Paginated } from "@/types";
+import { Head, Link, usePage } from "@inertiajs/vue3";
+
+type Student = {
+    user: {
+        id: number;
+        name: string;
+    };
+    score: number;
+    scores: {
+        score: string | null;
+    }[];
+};
+
+defineProps<{
+    students: Student[];
+}>();
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <div class="py-12">
+            <div class="container">
+                <p v-for="student in students" :key="student.user.id">
+                    {{ student.user.name }} ({{ student.score }})
+                </p>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
