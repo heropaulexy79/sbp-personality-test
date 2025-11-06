26ebe09e4b486fd8e8f6fb332a555ad137d941f9
heropaulexy <okeoluwaseun79@gmail.com>
2025-11-06 09:19:19 +0000

    new commits

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -5,16 +5,24 @@ import { createApp, h, DefineComponent } from "vue";
 import { createInertiaApp } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { ZiggyVue } from "../../vendor/tightenco/ziggy";
+import GlobalLayout from "./Layouts/GlobalLayout.vue";
 
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
+    resolve: async (name) => {
+        const page = await resolvePageComponent(
             `./Pages/${name}.vue`,
             import.meta.glob<DefineComponent>("./Pages/**/*.vue"),
-        ),
+        );
+
+        // This line tells Inertia to use GlobalLayout.vue as the default
+        // layout for any page that doesn't define its own.
+        page.default.layout = page.default.layout || GlobalLayout;
+        
+        return page;
+    },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
